381b408736407444b5f6ced18bae99e28c734660
ut-chase <chase@utail.co>
2019-01-25 13:54:33 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'utailBase',
-    version          = '0.0.11',
+    version          = '0.0.12',
     description      = 'unlimit tail basementlib',
     long_description = open('README.md').read(),
     author           = 'chase',
--- utail_base/time_support.py
+++ utail_base/time_support.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import datetime
 from dateutil import tz
+import pytz
 
-def localTimeToUTCTime(localDateTime):
-    localDT = localDateTime.replace(tzinfo=tz.tzlocal())
+def localTimeToUTCTime(localDateTime, localTimeZone = pytz.timezone('Asia/Seoul')):
+    # localDT = localDateTime.replace(tzinfo=tz.tzlocal())
+    localDT = localDateTime.replace(tzinfo=localTimeZone)
     return localDT.astimezone(tz.tzutc())
