a0f03ced8f63056553044c7816d2a6e981a70d45
ut-chase <chase@utail.co>
2019-01-28 22:04:42 +0900

    .

--- utail_base/selenium_pool.py
+++ utail_base/selenium_pool.py
@@ -30,6 +30,8 @@ class SeleniumPool(SeleniumPoolBaseClass, metaclass=SeleniumPoolSingleton):
 
         self._options = webdriver.ChromeOptions()
         self._options.add_argument('headless')
+        self._options.add_argument('no-sandbox')
+        self._options.add_argument('disable-dev-shm-usage')
         self._options.add_argument('window-size=1920x1080')
         self._options.add_argument("disable-gpu")
         self._options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
