d5cd71ccbd8ee2f25a124870371b463165ac1b33
ut-chase <chase@utail.co>
2019-01-28 18:14:55 +0900

    selenium

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'utailBase',
-    version          = '0.0.14',
+    version          = '0.0.15',
     description      = 'unlimit tail basementlib',
     long_description = open('README.md').read(),
     author           = 'chase',
--- utail_base/selenium_pool.py
+++ utail_base/selenium_pool.py
@@ -19,40 +19,67 @@ class SeleniumPoolSingleton(type):
 
 
 class SeleniumPool(SeleniumPoolBaseClass, metaclass=SeleniumPoolSingleton):
-    def __init__(self, webDriverPath, poolCnt=1):
+    def __init__(self, webDriverPath, poolCnt=0):
 
         self._lock = threading.Lock()
 
         self._poolCnt = poolCnt
         self._webDriverPath = webDriverPath
 
-        self._q = queue.Queue()
 
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
-        options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-        options.add_argument("lang=ko_KR")
+        self._options = webdriver.ChromeOptions()
+        self._options.add_argument('headless')
+        self._options.add_argument('window-size=1920x1080')
+        self._options.add_argument("disable-gpu")
+        self._options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+        self._options.add_argument("lang=ko_KR")
+
+        if 0 < self._poolCnt:
+            self._q = queue.Queue()
+
+            with self._lock:
+                for i in range(self._poolCnt):
+                    i
+
+                    log.debug('chrome driver loading... ')
+                    driver = webdriver.Chrome(self._webDriverPath, chrome_options=self._options)
+                    driver.implicitly_wait(5)
+
+                    self._q.put(driver)
+        else:
+            self._map = dict()
+
+    def createPool(self, threadID):
+        log.debug('chrome driver loading... ')
+
+        driver = webdriver.Chrome(self._webDriverPath, chrome_options=self._options)
 
         with self._lock:
-            for i in range(self._poolCnt):
-                i
+            self._map[str(threadID)] = driver
 
-                log.debug('chrome driver loading... ')
-                driver = webdriver.Chrome(self._webDriverPath, chrome_options=options)
-                driver.implicitly_wait(5)
+        driver.implicitly_wait(5)
 
-                self._q.put(driver)
-
-    def alloc(self):
+    def getDriver(self, threadID):
         with self._lock:
-            print('q size : {}'.format(self._q.qsize()))
-            return self._q.get()
+            return self._map[str(threadID)]
 
-    def free(self, driver):
-        with self._lock:
-            self._q.put(driver)
+    @staticmethod
+    def clearResource(driverPath):
+        driver = webdriver.Chrome(driverPath)
+        for handle in driver.window_handles:
+            driver.switch_to.window(handle)
+            driver.close()
+    
+        
+            
+    # def alloc(self):
+    #     with self._lock:
+    #         print('q size : {}'.format(self._q.qsize()))
+    #         return self._q.get()
+
+    # def free(self, driver):
+    #     with self._lock:
+    #         self._q.put(driver)
 
     
 
