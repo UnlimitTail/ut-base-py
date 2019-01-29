7891e7015d3b6a402fab560a696d71dcab4951c8
ut-chase <chase@utail.co>
2019-01-29 13:01:47 +0900

    .

--- utail_base/time_support.py
+++ utail_base/time_support.py
@@ -9,6 +9,6 @@ def localTimeToUTCTime(localDateTime, localTimeZone = pytz.timezone('Asia/Seoul'
     localDT = localDateTime.replace(tzinfo=localTimeZone)
     return localDT.astimezone(tz.tzutc())
 
-def timeOffset(localDateTime, offsetMinutes = -540):
-    lts = int(mktime(localDateTime.timetuple())) + (offsetMinutes * 60)
+def timeOffset(localDateTime, offsetMinutes = -540, offsetSecs = 0):
+    lts = int(mktime(localDateTime.timetuple())) + (offsetMinutes * 60) + offsetSecs
     return datetime.fromtimestamp(lts)
