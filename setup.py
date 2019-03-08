46465c06d5c412d033edc6ff450123c7b877bf7f
ut-chase <chase@utail.co>
2019-03-08 15:56:35 +0900

    inc ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'utailBase',
-    version          = '0.0.37',
+    version          = '0.0.38',
     description      = 'unlimit tail basementlib',
     long_description = open('README.md').read(),
     author           = 'chase',
--- utail_base/__init__.py
+++ utail_base/__init__.py
@@ -3,6 +3,7 @@ from .exception_decor import *
 from .hangul_support import *
 from .log_setup import *
 from .net_support import *
+from .nlp_support import *
 from .selenium_pool import *
 from .selenium_support import *
 from .string_support import *
