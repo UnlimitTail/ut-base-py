31e7b2b5894a4ef73c92118ee9c5d09b39a630ed
ut-chase <chase@utail.co>
2019-04-25 01:59:19 +0900

    raiseHttpError

--- utail_base/web_support.py
+++ utail_base/web_support.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup, NavigableString
+import inspect
 import json
 import logging
 import requests
@@ -69,6 +70,16 @@ class HttpError(Exception):
         return 'http error. code:{}. file:{} line:{} function:{} msg:{}'.format(self.code, 
          self.filename, self.line, self.function, self.message,
         )
+
+
+def raiseHttpError(log, status_code, msg=''):
+    previous_frame = inspect.currentframe().f_back
+    (filename, line_number, function_name, lines, index) = inspect.getframeinfo(previous_frame)
+
+    errmsg = 'request failed. file:{}({}). func:{}. code:{} / msg:{}'.format(filename, line_number, function_name, status_code, msg)
+    log.error(errmsg)
+    raise HttpError('failed request. msg:{}'.format(msg), filename, line_number,
+        function_name, status_code)
 
 
 
