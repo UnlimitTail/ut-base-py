8ec9d33de4ee12c483b63ad09a5cb9ded709dffb
ut-chase <chase@utail.co>
2019-06-25 21:36:31 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'utailBase',
-    version          = '0.0.69',
+    version          = '0.0.70',
     description      = 'unlimit tail basementlib',
     long_description = open('README.md').read(),
     author           = 'chase',
